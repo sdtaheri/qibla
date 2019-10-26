3bd3f5260eed2a65b9bd81605c6c3c2c1602c995
Saeed Taheri <saeed.taheri@gmail.com>
2019-10-26 18:51:46 +0330

    Initial Commit

---
+++ Qibla WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Qibla WatchKit Extension
+//
+//  Created by Saeed Taheri on 10/26/19.
+//  Copyright © 2019 Saeed Taheri. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
